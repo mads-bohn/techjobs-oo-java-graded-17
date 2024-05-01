98442303a6b11fdb3c5b8b8a4935e830696b2bd4
mads-bohn <mady.bohn@gmail.com>
2024-04-30 23:44:13 -0500

    wrote testSettingJobId

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,15 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job firstJob = new Job();
+        Job secondJob = new Job();
+        assertNotEquals(firstJob, secondJob);
+    }
 }
