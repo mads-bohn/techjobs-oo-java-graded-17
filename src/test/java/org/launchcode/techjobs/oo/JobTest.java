d538a4269bb936aa4e896c4a917bb246594eb75f
mads-bohn <mady.bohn@gmail.com>
2024-05-09 22:08:43 -0500

    fixed id issue in jobtest

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 
 public class JobTest {
     //TODO: Create your unit tests here
+
+    Job acmeJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
     @Test
     public void testSettingJobId() {
         Job firstJob = new Job();
@@ -15,22 +18,21 @@ public class JobTest {
 
     @Test
     public void testJobConstructorSetsAllFields() {
-        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         // assert value and class of name
-        assertEquals(job.getName(), "Product tester");
-        assertTrue(job.getName() instanceof String);
+        assertEquals(acmeJob.getName(), "Product tester");
+        assertTrue(acmeJob.getName() instanceof String);
         // assert value and class of employer
-        assertEquals(job.getEmployer().getValue(), "ACME");
-        assertTrue(job.getEmployer() instanceof Employer);
+        assertEquals(acmeJob.getEmployer().getValue(), "ACME");
+        assertTrue(acmeJob.getEmployer() instanceof Employer);
         // assert value and class of location
-        assertEquals(job.getLocation().getValue(), "Desert");
-        assertTrue(job.getLocation() instanceof Location);
+        assertEquals(acmeJob.getLocation().getValue(), "Desert");
+        assertTrue(acmeJob.getLocation() instanceof Location);
         // assert value and class of positionType
-        assertEquals(job.getPositionType().getValue(), "Quality control");
-        assertTrue(job.getPositionType() instanceof PositionType);
+        assertEquals(acmeJob.getPositionType().getValue(), "Quality control");
+        assertTrue(acmeJob.getPositionType() instanceof PositionType);
         // assert value and class of coreCompetency
-        assertEquals(job.getCoreCompetency().getValue(), "Persistence");
-        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(acmeJob.getCoreCompetency().getValue(), "Persistence");
+        assertTrue(acmeJob.getCoreCompetency() instanceof CoreCompetency);
     }
 
     @Test
@@ -42,9 +44,8 @@ public class JobTest {
 
     @Test
     public void testToStringStartsAndEndsWithNewLine() {
-        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        String testString = job.toString();
-        String firstChar = Character.toString(job.toString().charAt(0));
+        String testString = acmeJob.toString();
+        String firstChar = Character.toString(acmeJob.toString().charAt(0));
         String lastChar = Character.toString(testString.charAt(testString.length() - 1));
         assertEquals(firstChar, System.lineSeparator());
         assertEquals(lastChar, System.lineSeparator());
@@ -52,15 +53,14 @@ public class JobTest {
 
     @Test
     public void testToStringContainsCorrectLabelsAndData() {
-        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         String expectedString = System.lineSeparator() +
-                "ID: 1" + System.lineSeparator() +
+                "ID: 6" + System.lineSeparator() +
                 "Name: Product tester" + System.lineSeparator() +
                 "Employer: ACME" + System.lineSeparator() +
                 "Location: Desert" + System.lineSeparator() +
                 "Position Type: Quality control" + System.lineSeparator() +
                 "Core Competency: Persistence" + System.lineSeparator();
-        String actualString = job.toString();
+        String actualString = acmeJob.toString();
         assertEquals(expectedString, actualString);
     }
 
@@ -68,7 +68,7 @@ public class JobTest {
     public void testToStringHandlesEmptyField() {
         Job job = new Job("Product tester", new Employer("ACME"), new Location(""), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         String expectedString = System.lineSeparator() +
-                "ID: 1" + System.lineSeparator() +
+                "ID: 5" + System.lineSeparator() +
                 "Name: Product tester" + System.lineSeparator() +
                 "Employer: ACME" + System.lineSeparator() +
                 "Location: Data not available" + System.lineSeparator() +
